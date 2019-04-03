2298327486f54c1b77c77d423d1a446dfb28daf4
Shane D Wilson <me@shanedwilson.com>
2019-04-02 19:53:41 -0500

    Add project files.

---
+++ Bad Superbowl Namer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bad_Superbowl_Namer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
